a4396e0415e6e564c2dceb19a514bd355b2a4497
Supayan <dsupayan@gmail.com>
2026-06-30 11:21:19 +0530

    Implemented the functions to read the disk IO operation Stat.

--- src/disk.c
+++ src/disk.c
@@ -73,3 +73,42 @@ void total_disk_size(unsigned long num, SizeUnit *su)
         strcpy(su->unit, "GB");
     }
 }
+
+void read_disk_io_stat(IOStat *io,  int *count)
+{
+    FILE *fp;
+    char data[150];
+
+    fp = fopen("/proc/diskstats", "r");
+    if (fp == NULL)
+    {
+        perror("Error opening file : /proc/diskstats\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(data, sizeof(data), fp) != NULL)
+    {
+        sscanf(data, "%u %u %s %lu %lu %lu %u %lu %lu %lu %u %u %u",
+               &io->major, &io->minor, io->name, &io->read, &io->read_merged, &io->sectors_read, &io->read_time_ms, &io->write, &io->write_merged, &io->sectors_written, &io->write_time_ms, &io->io_in_progress, &io->io_in_ms);
+        io++;       
+        (*count)++;
+    }
+    fclose(fp);
+}
+void calculate_io(IOStat *io1, IOStat *io2, IOStat *result)
+{
+    strcpy(result->name,io1->name);
+
+    result->read = (io2->read - io1->read) * 512; // update the sector size for cross  device. Remove the hardcoded value and get it from /sys/block/sdX/queue/hw_sector_size
+    result->read_merged = (io2->read_merged - io1->read_merged) * 512;
+    result->sectors_read = (io2->sectors_read - io1->sectors_read) * 512;
+    result->read_time_ms = (io2->read_time_ms - io1->read_time_ms);
+
+    result->write = (io2->write - io1->write) * 512; // update the sector size for cross  device. Remove the hardcoded value and get it from /sys/block/sdX/queue/hw_sector_size
+    result->write_merged = (io2->write_merged - io1->write_merged) * 512;
+    result->sectors_written = (io2->sectors_written - io1->sectors_written) * 512;
+    result->write_time_ms = (io2->write_time_ms - io1->write_time_ms);
+
+    result->io_in_progress = (io2->io_in_progress-io1->io_in_progress);
+    result->io_in_ms = (io2->io_in_ms-io1->io_in_ms);
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include "../include/cpu.h"
 #include "../include/memory.h"
 #include "../include/disk.h"
@@ -44,5 +45,37 @@ int main()
         printf("Disk Space Avalible : %.2f %s\t", su.size, su.unit);
         printf("Avalible space : %lu\n\n", ds[i].avalible);
     }
+
+    IOStat io_stat1[10], io_stat2[10], result[10];
+    int io_count1 = 0, io_count2 =0, k=0;
+    read_disk_io_stat(io_stat1, &io_count1);
+    sleep(1);
+    read_disk_io_stat(io_stat2,&io_count2);
+
+    for (int i = 0; i <io_count1;i++){
+        for (int j = 0; j < io_count2;j++)
+        {
+            if(strcmp(io_stat1[i].name,io_stat2[j].name)==0){
+                calculate_io(&io_stat1[i],&io_stat2[j],&result[k++]);
+            }
+        }
+    }
+
+    for (int i =0;i<k;i++){
+        printf("Device Name : %s\t\n", result[i].name);
+        printf("Total Read : %lu\t\n", result[i].read);
+        printf("Total Read merged : %lu\t\n", result[i].read_merged);
+        printf("Total Read sector: %lu\t\n", result[i].sectors_read);
+        printf("Total Read MS: %d\t\n", result[i].read_time_ms);
+        printf("Total write : %lu\t\n", result[i].write);
+        printf("Total write merged : %lu\t\n", result[i].write_merged);
+        printf("Total write sector: %lu\t\n", result[i].sectors_written);
+        printf("Total write ms: %d\t\n", result[i].write_time_ms);
+        printf("Total io : %d\t\n", result[i].io_in_progress);
+        printf("Total io ms: %d\t\n", result[i].io_in_ms);
+        
+        
+    }
+
     return 0;
 }
